e8ba527a98d38c567c905c7d6274a74fedfab4ff
Morten Hofft <mortenhoefft@gmail.com>
2018-03-14 22:43:06 +0100

    cors header

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fetchUrl = require("fetch").fetchUrl;
 const PORT = process.env.PORT || 5000;
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
