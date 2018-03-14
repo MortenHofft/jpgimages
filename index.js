b5d8660194457f0898c882019a4dfff2fa8f6efd
Morten Hofft <mortenhoefft@gmail.com>
2018-03-14 22:42:51 +0100

    cors header

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const getUrls = require('get-urls');
 var fetchUrl = require("fetch").fetchUrl;
 const PORT = process.env.PORT || 5000;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res){
   res.send('Hello World!')
 });
